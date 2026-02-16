53e1e007084e16cf56169ed3ab29d3541681bf46
notadmg <157731068+notadmg@users.noreply.github.com>
2026-02-16 19:22:39 +0400

    Update smoothliquids.js

--- mods/smoothliquids.js
+++ mods/smoothliquids.js
@@ -1,4 +1,11 @@
 //smoothliquids.js mod for sandboxels by nota
+//water
+elements.water.behavior = [
+    "XX|XX|XX",
+    "XX|XX|M2 AND BO",
+    "XX|M1|M2",
+];
+elements.water.flippableX = true;
 //salt_water
 elements.salt_water.behavior = [
     "XX|XX|XX",
