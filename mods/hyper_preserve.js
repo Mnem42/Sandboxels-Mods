91f7fe93346955d41312d2c86b491ee2d5177d7f
mnem <mnem424242@gmail.com>
2026-09-03 17:48:36 +0100

    feat(hyper_preserve.js): patch save options

--- mods/hyper_preserve.js
+++ mods/hyper_preserve.js
@@ -40,6 +40,23 @@ function patch() {
                 </button>
             </span>`,
         );
+
+    document
+        .querySelector("#savePromptParent .toggles-row")
+        .insertAdjacentHTML(
+            "beforeend",
+            `<span>
+                <input
+                    type="button"
+                    value="Save hyperboxels"
+                    class="toggleInput"
+                    onclick="toggleInput(this,undefined,false)"
+                    state="0"
+                    id="saveHyper"
+                    style="display: inline-block;"
+                />
+            </span>`
+        )
 }
 
 function isolate_hyper(pixmap) {
@@ -63,11 +80,109 @@ function isolate_hyper(pixmap) {
     return hyper;
 }
 
+window.confirmSave = (confirmed = 0) => {
+    if (!savingState) { return }
+    if (confirmed < 1 && savingState.slot && settings.resetwarning!==0 && localStorage["SandboxelsSaves/"+savingState.slot]) {
+        let tempState = savingState;
+        promptConfirm("Are you sure you want to overwrite the current save in slot "+savingState.slot+"? This cannot be undone.",
+            (r) => {
+                savingState = tempState;
+                if (r) confirmSave(1);
+            },
+            "Overwrite Slot"
+        )
+        return;
+    }
+
+    // might as well change this stuff a bit, seeing as it'll break anyway if there's an update
+
+    const get_en = (id) => document.getElementById(id).getAttribute("state") === "1";
+
+    const config = {
+        name   : document.getElementById("saveName").value || "Unnamed",
+        author : document.getElementById("saveAuthor").value || "",
+
+        temp      : get_en("saveTemp"),
+        colors    : get_en("saveColors"),
+        mods      : get_en("saveMods"),
+        raw       : get_en("saveRaw"),
+        save_hyper: get_en("saveHyper"),
+
+        keep: [],
+        tags: []
+    }
+
+    if (config.author && !currentSaveData.loaded) {
+        setSetting("authorName", config.author)
+    }
+
+    if (config.temp) { config.keep.push("temp") }
+    if (config.colors) { config.keep.push("color") }
+
+    var saveDesc = document.getElementById("saveDesc").value ?? "";
+    var saveTagsList = document.getElementById("saveTags");
+    var enabledTags = saveTagsList.querySelectorAll('input[state="1"]');
+    enabledTags.forEach((tagElem) => {
+        config.tags.push(parseInt(tagElem.getAttribute("data-tag")));
+    })
+
+    console.log(config)
+    if (standaloneType === "steam") {
+        if (savingState.overwrite) {
+            overwriteSaveFileConfirm(savingState.overwrite,generateSave(undefined,config))
+        }
+        else {
+            writeSaveFile(generateSave(undefined,config));
+        }
+        closeMenu();
+    }
+    else if (savingState.slot) {
+        // save stringified generateSave() to localStorage SandboxelsSaves/i
+        localStorage.setItem("SandboxelsSaves/"+savingState.slot, JSON.stringify(generateSave(undefined,config)));
+        closeMenu();
+        showSaves();
+    }
+    else {
+        // save to <name>.sbxls
+        var save = generateSave(undefined,config);
+        var blob = new Blob([JSON.stringify(save)], {type: "application/json"});
+        var url = URL.createObjectURL(blob);
+        var a = document.createElement("a");
+        a.href = url;
+        a.download = config.name+".sbxls";
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+        closeMenu();
+    }
+
+    if (currentSaveData) {
+        if (savingState && !isNaN(savingState.slot))
+            currentSaveData.slot = savingState.slot;
+
+        currentSaveData.name = config.name;
+        currentSaveData.author = save.author;
+        currentSaveData.desc = saveDesc;
+    }
+}
+
+const ssp_old = showSavePrompt
+window.showSavePrompt = () => {
+    document
+        .getElementById("saveHyper")
+        .setAttribute("state", settings.save_hyper)
+
+    ssp_old()
+}
+
 const gsave_old = generateSave;
 window.generateSave = (pixmap, opts) => {
     const generated = gsave_old(pixmap, opts);
 
-    if (settings.hyperpreserve) {
+    if (settings.hyperpreserve || opts.save_hyper) {
         generated.hyper = isolate_hyper(pixmap ?? pixelMap);
     }
 
